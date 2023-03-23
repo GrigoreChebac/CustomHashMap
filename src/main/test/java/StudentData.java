72bac43dd561974f17da46286d025db4bb672bc5
GrigoreChebac <grigore.chebac@poly.com>
2023-03-23 17:28:13 +0200

    a bit of tests

---
+++ src/main/test/java/StudentData.java
@@ -0,0 +1,18 @@
+import com.endava.internship.collections.Student;
+
+import java.time.LocalDate;
+
+public class StudentData {
+    Student student1 = new Student("Grigore1 ", LocalDate.of(1999, 1, 23), "A young man");
+    Student student2 = new Student("Kolea2 ", LocalDate.of(1934, 2, 3), "String man");
+    Student student3 = new Student("Kate3 ", LocalDate.of(2019, 3, 6), "Teensdsafager");
+    Student student4 = new Student("John4 ", LocalDate.of(2029, 4, 21), "Teenasfasager");
+    Student student5 = new Student("Iron5 ", LocalDate.of(2039, 5, 7), "asasdf");
+    Student student6 = new Student("ssIron6 ", LocalDate.of(2039, 5, 7), "a123sf");
+    Student student7 = new Student("qqIron7 ", LocalDate.of(2039, 5, 7), "aasd12zcsf");
+    Student student8 = new Student("qqIron8 ", LocalDate.of(2039, 5, 7), "asxfhdf");
+    Student student9 = new Student("qqIron9 ", LocalDate.of(2039, 5, 7), "asdhcnf");
+    Student student10 = new Student("qqIron10 ", LocalDate.of(2039, 5, 7), "asfhdf");
+    Student student11 = new Student("qqIron11 ", LocalDate.of(2039, 5, 7), "asddgf");
+
+}
